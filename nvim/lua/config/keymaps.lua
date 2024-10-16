77bca8354ff910d26fe079cba16a5fcab2e8d72c
saad-out <outsaad03@gmail.com>
2024-10-16 12:43:48 +0100

    reset everything

--- nvim/lua/config/keymaps.lua
+++
@@ -1,6 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
--- Mapping to open terminal
-vim.api.nvim_set_keymap("n", "<Leader>t", ":terminal<CR>", { noremap = true, silent = true })
