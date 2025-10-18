8f4970e611db94055af2669cdfd2aea8fb458172
saad-out <outsaad03@gmail.com>
2025-10-18 03:22:08 +0100

    change copilot default key

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -4,3 +4,9 @@
 -- vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
 vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
 vim.api.nvim_set_keymap("n", "<Leader>t", ":terminal<CR>", { noremap = true, silent = true })
+
+-- Copilot
+-- Disable default <Tab> mapping
+vim.g.copilot_no_tab_map = true
+-- Map <C-j> to accept Copilot suggestion
+vim.api.nvim_set_keymap("i", "<C-j>", 'copilot#Accept("<CR>")', { expr = true, silent = true, noremap = true })
